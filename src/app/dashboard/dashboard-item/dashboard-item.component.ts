fbe9006dd1bae665c776cf21abc16b891a87065b
Ricsius <hajnal.ricsi@gmail.com>
2025-01-28 22:39:03 +0100

    Add dashboard-item component

---
+++ src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-item.component.html',
+  styleUrl: './dashboard-item.component.css'
+})
+export class DashboardItemComponent {
+  @Input({ required: true }) image!: ImageData;
+  @Input({ required: true }) title!: string;
+}
+
+type ImageData = {
+  src: string,
+  alt: string
+}
